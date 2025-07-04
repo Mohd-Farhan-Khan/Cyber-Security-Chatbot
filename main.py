5c2ab337cd2aa929b47a7d1a3bb9500d1bc6f330
Mohd Farhan Khan <goldenstone447@gmail.com>
2025-07-04 16:28:30 +0530

    Removed the CLI feature

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ from src.text_splitter import split_documents
 from src.embeddings import create_faiss_vectorstore
 from src.rag_pipeline import setup_rag_pipeline
 from src.interfaces.streamlit_app import run_streamlit_app
-from src.interfaces.cli_app import run_chatbot_cli
 
-MODE = "streamlit"  # Change to "cli" if needed
-
-if MODE == "streamlit":
+if __name__ == "__main__":
     run_streamlit_app()
-else:
-    file_path = "data/financial_services_cyber_threats_dataset.pdf"
-    docs = load_documents(file_path)
-    chunks = split_documents(docs)
-    vectorstore = create_faiss_vectorstore(chunks)
-    qa_chain = setup_rag_pipeline(vectorstore)
-    run_chatbot_cli(qa_chain)
